e18a93d14ed2336b0860ea6eefd87c465fefdbfd
smartdriver321 <tinggaldidisneyland@gmail.com>
2025-01-28 15:29:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# NextJS 15 Authentication Tutorial
+
+Simple implementation of Auth.js with Prisma in Next.js 15
+
+## Requirements
+
+- Node.js 22+
+
+## Quick Start
+
+1. **Clone the repository**
+
+```bash
+# For tutorial
+git clone -b starter https://github.com/codegenixdev/auth-nextjs-tutorial.git
+
+# For complete code
+git clone https://github.com/codegenixdev/auth-nextjs-tutorial.git
+```
+
+2. **Setup environment for complete code**
+
+```bash
+cp .env.sample .env
+# Update .env with your values
+```
+
+3. **Install & Run**
+
+```bash
+npm install
+npm run db:migrate
+npm run dev
+```
+
+## Branches
+
+- `starter`: Initial setup
+- `master`: Complete implementation
+
+## Tech Stack
+
+- Next.js 15
+- Node.js 22
+- TypeScript
+- Prisma
+- Auth.js
+
+---
+
+Happy coding! 🚀
---
+++ src/lib/auth.ts
@@ -0,0 +1,72 @@
+import NextAuth from 'next-auth'
+import { v4 as uuid } from 'uuid'
+import GitHub from 'next-auth/providers/github'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { encode as defaultEncode } from 'next-auth/jwt'
+import Credentials from 'next-auth/providers/credentials'
+
+import db from '@/lib/db/db'
+import { schema } from '@/lib/schema'
+
+const adapter = PrismaAdapter(db)
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+	adapter,
+	providers: [
+		GitHub,
+		Credentials({
+			credentials: {
+				email: {},
+				password: {},
+			},
+			authorize: async (credentials) => {
+				const validatedCredentials = schema.parse(credentials)
+
+				const user = await db.user.findFirst({
+					where: {
+						email: validatedCredentials.email,
+						password: validatedCredentials.password,
+					},
+				})
+
+				if (!user) {
+					throw new Error('Invalid credentials.')
+				}
+
+				return user
+			},
+		}),
+	],
+	callbacks: {
+		async jwt({ token, account }) {
+			if (account?.provider === 'credentials') {
+				token.credentials = true
+			}
+			return token
+		},
+	},
+	jwt: {
+		encode: async function (params) {
+			if (params.token?.credentials) {
+				const sessionToken = uuid()
+
+				if (!params.token.sub) {
+					throw new Error('No user ID found in token')
+				}
+
+				const createdSession = await adapter?.createSession?.({
+					sessionToken: sessionToken,
+					userId: params.token.sub,
+					expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+				})
+
+				if (!createdSession) {
+					throw new Error('Failed to create session')
+				}
+
+				return sessionToken
+			}
+			return defaultEncode(params)
+		},
+	},
+})
